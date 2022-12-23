858f1586bfaa67795d54cb7d3bf281d35118742b
Dounia Rtaili <drtaili@e1r5p15.1337.ma>
2022-12-23 02:59:21 +0100

    added

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: drtaili <drtaili@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 03:17:19 by drtaili           #+#    #+#             */
-/*   Updated: 2022/12/23 01:46:16 by drtaili          ###   ########.fr       */
+/*   Updated: 2022/12/23 02:10:38 by drtaili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,32 @@
 //                                  1          j >> 7 => 0000000|1| && 1 = 1 sigusr1
 //                                   0         j >> 6 => 0000000|0| && 1 = 0 sigusr2
 //                                    0        j >> 5 => 0000000|0| && 1 = 0 sigusr2
+int	ft_atoi(const char *str)
+{
+	int	r;
+	int	i;
+	int	s;
+
+	i = 0;
+	r = 0;
+	s = 1;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			s = s * (-1);
+		i++;
+	}
+	while (str[i] <= '9' && str[i] >= '0')
+	{
+		r = r * 10 + (str[i] - '0');
+		i++;
+	}
+	return (r * s);
+}
+
 void send(char *str, int pip)
 {
     int i;
@@ -45,7 +71,7 @@ int main(int argc, char *argv[])
     int res;
     if (argc == 3)
     {
-        if (argv[1] >= 0)
+        if (ft_atoi(argv[1]) >= 0)
         {
             send(argv[2], argv[1]);
         }
--- server.c
+++ server.c
@@ -6,23 +6,57 @@
 /*   By: drtaili <drtaili@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 05:59:59 by drtaili           #+#    #+#             */
-/*   Updated: 2022/12/23 02:00:54 by drtaili          ###   ########.fr       */
+/*   Updated: 2022/12/23 02:45:36 by drtaili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int handler_sigusr2()
+int	ft_strlenint(long n)
 {
-    return (0);
+	int	i;
+
+	i = 0;
+	while (n / 10 != 0)
+	{
+		n = n / 10;
+		i++;
+	}
+	if (n < 0)
+		i++;
+	return (i + 1);
 }
 
-int handler_sigusr1()
+char	*ft_itoa(int n)
 {
-    return (1);
-}
+	int		i;
+	char		*a;
+	long		nb;
 
+	nb = (long)n;
+	i = ft_strlenint(nb);
+	a = malloc(ft_strlenint(nb) + 1);
+	if (a == NULL)
+		return (NULL);
+	a[i--] = 0;
+	if (nb == 0)
+		a[0] = '0';
+	if (nb < 0)
+	{
+		a[0] = '-';
+		nb = nb * -1;
+	}
+	while (nb > 0)
+	{
+		a[i] = (nb % 10) + 48;
+		nb = nb / 10;
+		i--;
+	}
+	return (a);
+}
 void    handler(int signum)
 {
     static int  cpt;
@@ -36,15 +70,21 @@ void    handler(int signum)
     cpt++;
     if (cpt == 8)
     {
-        write(1, ch, 1);
+        write(1, &ch, 1);
         ch = 0;
         cpt = 0;
     }
 }
-void    function()
+
+int main()
 {
     struct sigaction sa;
     sa.sa_handler = handler;
     sigaction(SIGUSR1, &sa, NULL);
     sigaction(SIGUSR2, &sa, NULL);
+    write(1, ft_itoa(getpid()), ft_strlenint(getpid()));
+    while (1)
+    {
+        wait(NULL);
+    }
 }
